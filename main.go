f584a4797f97e7d1de26a1f6a8d59cbc772cc4e9
Forest <767425412lin@gmail.com>
2022-06-22 06:35:13 +0800

    feat: add cors config

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
